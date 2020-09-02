797ad0df80008acdcf900625c9367282e6085436
Peter Hencz <peter.hencz@gmail.com>
2020-09-02 14:52:07 +0200

    save

---
+++ project_02/app.ts
@@ -0,0 +1,13 @@
+
+let age: number;
+age = 30;
+
+const userName = "KISCIca";
+
+console.log(userName, age);
+
+const button: any = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  console.log("clik");
+});
